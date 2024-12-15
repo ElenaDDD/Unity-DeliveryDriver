6040e38f29698a5cef09184a9d4a7de23aa437df
Olena <dobromyselena@gmail.com>
2024-12-15 17:17:14 +0100

    Added boost and bumps

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,6 +5,12 @@ public class Driver : MonoBehaviour
     [SerializeField] float steerSpeed = 200f;
     [SerializeField] float moveSpeed = 15f;
 
+
+    private float SpeedMultiplier = 1.0f;
+    private float boostedSpeed = 30f;
+    private float slowSpeed = 10f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,5 +24,19 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+             moveSpeed = boostedSpeed;
+        }
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
     }
 }
